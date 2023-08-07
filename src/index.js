4cee9868d7130b8499e88189d6a027c87a4ff3d4
Shizan Shaikh <51877872+therealshizan@users.noreply.github.com>
2023-08-07 13:22:34 +0530

    SwiftGlide Init

--- src/index.js
+++ src/index.js
@@ -1,39 +1,77 @@
-const sliderWrap = document.querySelector(".slider-wrap");
-const sliderPrevBtn = document.querySelector(".slider-prev-btn");
-const sliderNextBtn = document.querySelector(".slider-next-btn");
+const sliderEl = document.querySelector(".swift-glides");
+const slides = document.querySelectorAll(".swift-glide-slide");
+const sliderPrevBtn = document.querySelector(".glide-prev-btn");
+const sliderNextBtn = document.querySelector(".glide-next-btn");
+
 
 class Slider {
-  constructor(sliderWrap, prevBtn, nextBtn) {
-    this.sliderWrap = sliderWrap;
-    this.prevBtn = prevBtn;
-    this.nextBtn = nextBtn;
+  constructor(sliderObj) {
+    this.sliderObj = sliderObj
+    const {slides, slidesPerView, mobileSlidesPerView, tabletSlidesPerView} = sliderObj
+
+    // Vertical Slider
+    if(this.sliderObj.type === 'mousewheel'){
+      this.sliderObj.sliderMain.style.display = 'grid'
+      this.sliderObj.sliderMain.style.height = '50vh'
+      this.sliderObj.sliderMain.style.scrollSnapType = 'y mandatory'
+    }
+
+    // Media Queries
+    const tablet = window.matchMedia('(max-width: 768px)');
+    const mobile = window.matchMedia('(max-width: 576px)');
+
+    // Slides Per View
+    slides.forEach(slide => {
+      let slideSize = mobile.matches && 100 / mobileSlidesPerView || tablet.matches && 100 / tabletSlidesPerView || 100 / slidesPerView
+      slide.style.flex = `0 0 ${slideSize}%`
+    });
   }
 
+  // Next Slide
   goToNextSlide() {
     const scrollLeftOptions = {
-      left: sliderWrap.scrollLeft + 640,
+      left: this.sliderObj.sliderMain.scrollLeft + 640,
       behavior: "smooth"
     };
-    this.sliderWrap.scroll(scrollLeftOptions);
-    console.log("Next");
+    this.sliderObj.sliderMain.scroll(scrollLeftOptions);
   }
 
+  // Previous Slide
   goToPrevSlide() {
     const scrollRightOptions = {
-      left: sliderWrap.scrollLeft - 640,
+      left: this.sliderObj.sliderMain.scrollLeft - 640,
       behavior: "smooth"
     };
-    this.sliderWrap.scroll(scrollRightOptions);
-    console.log("Next");
+    this.sliderObj.sliderMain.scroll(scrollRightOptions); 
   }
+  
 }
 
-const slider = new Slider(sliderWrap, sliderPrevBtn, sliderNextBtn);
 
-sliderNextBtn?.addEventListener("click", () => {
+// Slider Options You Can Also Direct Pass It As Argument
+const slideOptions = {
+  sliderMain: sliderEl,
+  slides: slides,
+  prevBtn: sliderPrevBtn,
+  nextBtn: sliderNextBtn,
+  slidesPerView: 2,
+  mobileSlidesPerView: 1,
+  tabletSlidesPerView: 2,
+  type: 'mousewheel'
+}
+
+
+// Slider Instance
+const slider = new Slider(slideOptions);
+
+
+// Next Button Event
+slider.sliderObj.nextBtn?.addEventListener("click", () => {
   slider.goToNextSlide();
 });
 
-sliderPrevBtn?.addEventListener("click", () => {
+
+// Previous Button Event
+slider.sliderObj.prevBtn?.addEventListener("click", () => {
   slider.goToPrevSlide();
 });
